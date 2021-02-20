f00266d0e969bfb7ba38e7cdced708662ab4c384
m-iriyam <m-iriyam@m-iriyam.local>
2021-02-20 21:56:37 +0900

    init after

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        new Math().resetNumMin(true);
+        new Math().resetMinNum(true);
     }
 }
--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -1,10 +1,22 @@
 public class Math {
-    int numMin;
+    int minNum;
     int x;
-    public void resetNumMin(boolean b) {
+    public void resetMinNum(boolean b) {
         if(b) {
             x = 0;
         }
-        numMin = 0;
+        minNum = 0;
+    }
+    public void changeX(){
+        x=0;
+        x=1;
+        x=2;
+        x=3;
+        x=4;
+        x=5;
+        x=6;
+        x=7;
+        x=8;
+        x=9;
     }
 }
