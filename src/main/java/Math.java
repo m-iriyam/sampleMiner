5b7ae9bdc0965ed1625f4f5f5ebe1ce116fe8cb8
m-iriyam <m-iriyam@m-iriyam.local>
2021-02-20 22:14:24 +0900

    init before

--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -1,22 +1,10 @@
 public class Math {
-    int minNum;
+    int numMin;
     int x;
-    public void resetMinNum(boolean b) {
+    public void resetNumMin(boolean b) {
         if(b) {
             x = 0;
         }
-        minNum = 0;
-    }
-    public void changeX(){
-        x=0;
-        x=1;
-        x=2;
-        x=3;
-        x=4;
-        x=5;
-        x=6;
-        x=7;
-        x=8;
-        x=9;
+        numMin = 0;
     }
 }
